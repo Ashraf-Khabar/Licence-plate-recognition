93db110a80665e4181712675e305e8e0a7937508
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-28 16:59:58 +0100

    commit

--- api/main.py
+++ api/main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # templates_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
-    return render_template(os.path.join('index.html'))
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
