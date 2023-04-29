208411e8196ce2db5575de4558f18378b4f4a469
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-29 21:56:12 +0100

    Automated commit at Sat Apr 29 21:56:12 MST 2023

--- api/push.sh
+++ api/push.sh
@@ -2,5 +2,6 @@
 
 git checkout api   # switch to the "api" branch
 git add .          # add all changes
+git pull origin api
 git commit -m "Automated commit at $(date)"   # commit changes with a timestamp
 git push origin api   # push changes to the "api" branch on the remote repository
