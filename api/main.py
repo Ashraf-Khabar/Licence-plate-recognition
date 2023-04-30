abb5919cbc924c8c8c04bd4810f083c98ce750b6
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-30 21:00:41 +0100

    Customize the designe of the email html page

--- api/main.py
+++ api/main.py
@@ -50,17 +50,72 @@ def send_email():
         msg = MIMEMultipart()
         msg['From'] = 'ashrafkhabaradm@gmail.com'
         msg['To'] = user_email
-        msg['Subject'] = 'Test Email'
-        body = f"""
-            <html>
-            <head></head>
+        msg['Subject'] = 'Email Notification from RADAR.AI : Your vehicule is captured'
+        body = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy1Thrge38hDQT9Xb0a1rKGfGgpeI8U27d8xEU" crossorigin="anonymous">
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+                <title>Email from RADAR.AI</title>
+                            <style>
+                body{margin-top:20px;}
+                .mail-seccess {
+                    text-align: center;
+                    background: #fff;
+                    border-top: 1px solid #eee;
+                }
+                .mail-seccess .success-inner {
+                    display: inline-block;
+                }
+                .mail-seccess .success-inner h1 {
+                    font-size: 100px;
+                    text-shadow: 3px 5px 2px #3333;
+                    color: #006DFE;
+                    font-weight: 700;
+                }
+                .mail-seccess .success-inner h1 span {
+                    display: block;
+                    font-size: 25px;
+                    color: #333;
+                    font-weight: 600;
+                    text-shadow: none;
+                    margin-top: 20px;
+                }
+                .mail-seccess .success-inner p {
+                    padding: 20px 15px;
+                }
+                .mail-seccess .success-inner .btn{
+                    color:#fff;
+                }
+            </style>
+            </head>
             <body>
-                <h1>Email from RADAR.AI</h1>
-                <p>Hello,</p>
-                <p>This is a test email from RADAR.AI.</p>
-                <p>Thank you for using our service.</p>
+                
+                <section class="mail-seccess section">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 offset-lg-3 col-12">
+                                <!-- Error Inner -->
+                                <div class="success-inner">
+                                    <h1><i class="fa fa-envelope"></i><span>This is email from RADAR.AI</span></h1>
+                                    <p>Thank you for using our service.</p>
+                                    <a href="http://localhost:5000/" class="btn btn-primary btn-lg">Go Home</a>
+                                </div>
+                                <!--/ End Error Inner -->
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
             </body>
-            </html>
+            
         """
         msg.attach(MIMEText(body, 'html'))
         # Connect to the SMTP server and send the message
