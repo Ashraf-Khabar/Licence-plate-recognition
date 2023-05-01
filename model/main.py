c7ec0a8830451f4343f430cfe904d3c50baaa2bd
IDRISSI-Karim <126118945+IDRISSI-Karim@users.noreply.github.com>
2023-05-01 23:18:42 +0200

    Automated commit at Mon, May  1, 2023 11:18:42 PM

--- model/main.py
+++ model/main.py
@@ -1,3 +1,4 @@
 # This is the main project, here where we can create our project, our functions, our class
 # We can use either '.ipynb' or '.py'
 # This is the project where I can create the project
+hhhhhhhhhh
