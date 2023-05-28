fd4ac6d669e0774af013503991bd96866ad61cff
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-05-28 23:17:43 +0100

    adding some data

--- api/test.py
+++ api/test.py
@@ -1,5 +1,5 @@
-from api.model import Model
+from model import Model
 
-img_processor = Model("data/test.png")
+img_processor = Model("data/test4.png")
 
 print (f"the license plate number is: {img_processor.get_plate_number()}")
