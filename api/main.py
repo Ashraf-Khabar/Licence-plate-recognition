aa5619bd0ec14307094fdc90d3b270ab7a6a686c
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-06-15 06:20:31 +0100

    Creation of an interactive user interface

--- api/main.py
+++ api/main.py
@@ -5,12 +5,15 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 import os
+from flask import Flask, render_template, request
+from model import Model
 
 # Loading the file .env in order to use :
 load_dotenv()
 
 # Init the Flask API app :
 app = Flask(__name__)
+img_processor = Model("data/Cars389.png")
 
 # Configure the ORM with the connection string for ORACLE DB .
 # The connection parameters are loaded by the .env file .
@@ -37,102 +40,74 @@ class User(db.Model):
     user_licence_plate = db.Column(db.String(50))
     user_city = db.Column(db.String(50))
     user_car_type = db.Column(db.String(50))
+    
+
+@app.route('/process_license_plate', methods=['POST'])
+def process_license_plate():
+    # Retrieve the uploaded image from the request
+    file = request.files['image']
+    # Save the image to a temporary file
+    image_path = 'temp_image.jpg'
+    file.save(image_path)
+    # Process the image using the license plate detection model
+    img_processor = Model(image_path)
+    license_plate_number = img_processor.get_plate_number()
+    # Query the database for a user with the given license plate number
+    user = User.query.filter_by(user_licence_plate=license_plate_number).first()
+    # Delete the temporary image file
+    os.remove(image_path)
+    message = []
+    if user is None:
+        message.append('User not found')
+    else:
+        message.append(user.user_name)
+        message.append(user.user_licence_plate)
+        message.append(user.user_city)
+        message.append(user.user_mail)
+        # Send email to the user
+        send_email(user.user_mail, license_plate_number, user.user_city, user.user_name)
+    return render_template('index.html', license_plate=license_plate_number, message=message)
 
 
-# The POST API in order to send mails to user :
-@app.route('/send_email', methods=['POST'])
-def send_email():
-    # user_email = request.form['user_email']
-    user_email = 'khabarachraf@gmail.com'
+def send_email(user_email, license_plate, city, name):
     try:
-        # Send email to user
-        # Construct the message
+        # Construct the email message
         msg = MIMEMultipart()
         msg['From'] = 'ashrafkhabaradm@gmail.com'
         msg['To'] = user_email
-        msg['Subject'] = 'Email Notification from RADAR.AI : Your vehicule is captured'
-        body = """
-            <!DOCTYPE html>
-            <html lang="en">
+        msg['Subject'] = 'Email Notification from RADAR.AI: Your vehicle is captured'
+        
+        # Construct the email body with the user's information
+        body = f"""
+            <html>
             <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy1Thrge38hDQT9Xb0a1rKGfGgpeI8U27d8xEU" crossorigin="anonymous">
-                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-                <title>Email from RADAR.AI</title>
-                            <style>
-                body{margin-top:20px;}
-                .mail-seccess {
-                    text-align: center;
-                    background: #fff;
-                    border-top: 1px solid #eee;
-                }
-                .mail-seccess .success-inner {
-                    display: inline-block;
-                }
-                .mail-seccess .success-inner h1 {
-                    font-size: 100px;
-                    text-shadow: 3px 5px 2px #3333;
-                    color: #006DFE;
-                    font-weight: 700;
-                }
-                .mail-seccess .success-inner h1 span {
-                    display: block;
-                    font-size: 25px;
-                    color: #333;
-                    font-weight: 600;
-                    text-shadow: none;
-                    margin-top: 20px;
-                }
-                .mail-seccess .success-inner p {
-                    padding: 20px 15px;
-                }
-                .mail-seccess .success-inner .btn{
-                    color:#fff;
-                }
-            </style>
+                <!-- Add any necessary styles or scripts here -->
             </head>
             <body>
-                
-                <section class="mail-seccess section">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-6 offset-lg-3 col-12">
-                                <!-- Error Inner -->
-                                <div class="success-inner">
-                                    <h1><i class="fa fa-envelope"></i><span>This is email from RADAR.AI</span></h1>
-                                    <p>Thank you for using our service.</p>
-                                    <a href="http://localhost:5000/" class="btn btn-primary btn-lg">Go Home</a>
-                                </div>
-                                <!--/ End Error Inner -->
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                <h2>License Plate Information:</h2><br>
+                <p>License Plate: {license_plate}</p><br>
+                <p>City: {city}</p><br>
+                <p>Car Type: {name}</p><br>
+                <!-- Add any additional user information here -->
             </body>
-            
+            </html>
         """
         msg.attach(MIMEText(body, 'html'))
+        
         # Connect to the SMTP server and send the message
         smtp_server = os.getenv('SMTP_SERVER')
         smtp_port = os.getenv('SMTP_PORT')
         smtp_username = os.getenv('SMTP_USERNAME')
         smtp_password = os.getenv('SMTP_PASSWORD')
+        
         with smtplib.SMTP(smtp_server, smtp_port) as server:
             server.starttls()
             server.login(smtp_username, smtp_password)
             server.sendmail(smtp_username, user_email, msg.as_string())
-        return jsonify({'status': 'OK'})
+        
+        print("Email sent successfully")
     except Exception as e:
-        return jsonify({
-            'error': e,
-            'status': 'Not OK'
-        })
+        print("Error sending email:", e)
 
 
 # The get API in order to render a template named 'users.html' .
@@ -149,8 +124,8 @@ def get_users():
 # The get API in order to render a template named 'index.html' .
 # The index field .
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', license_plate='', message='')
 
 
 # The main function in order to run the project in the PORT: 5000 and the server localhost .
